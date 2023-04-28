a03c07fd47ea7c7e2bb232da397b1129fc59d6f3
Nada Soliman <nsoliman@andrew.cmu.edu>
2023-04-28 14:20:18 +0300

    Add rule_DDO json, create rule and apply_rule code

--- src/main.py
+++ src/main.py
@@ -5,15 +5,12 @@ import pymongo
 import rule_engine
 import scheduler
 
-def postgres_connect(db_name):
+def postgres_connect(db_connection_str):
     # establishing the connection
-    conn = psycopg2.connect(
-    database=db_name, user='postgres', password='password', host='127.0.0.1', port= '5432')
+    conn = psycopg2.connect(db_connection_str)
     conn.autocommit = True
 
-    # creating a cursor object using the cursor() method
-    user_db = conn.cursor()
-    return user_db
+    return conn
 
 def main():
 
@@ -22,17 +19,16 @@ def main():
     args = parser.parse_args()
 
     mongo_db = pymongo.MongoClient("mongodb://localhost:27017/")["rule_engine_db"]
-
-    # Todo: 
-    # - connect to the the mongoDB database
-    # - call create_rule from rule_engine
-    # - call rule_scheduler from rule_scheduler
-
     rule_filepath = args.fp
     rule_obj = rule_engine.create_rule(mongo_db, rule_filepath)
 
-    user_db = postgres_connect(rule_obj["db_name"])
+    conn = postgres_connect(rule_obj["db_connection_str"])
+    user_db = conn.cursor()
     scheduler.scheduler(mongo_db, user_db, rule_obj)
+
+    mongo_db.close()
+    user_db.close()
+    conn.close()
 
     pass
 
--- src/rule_engine.py
+++ src/rule_engine.py
@@ -1,14 +1,23 @@
-def create_rule(mongo_db, filename):
-    # Todo:
-    # - create the rule document from json and add it to the collection
+import json
 
-    pass
+# create the rule document from json and add it to the Rules collection
+def create_rule(mongo_db, rule_filepath):
+    try:
+        f = open(rule_filepath, 'r')
+        rule = json.load(f)
+        f.close()
 
+        rule_obj = mongo_db['Rules'].insert_one(rule)
+        return rule_obj
+
+    except OSError:
+        raise "OSError: Couldn't open the rule file\n"
+
+# apply the sql query on the postgresSQL database and return the matches
 def apply_rule_on_database(user_db, sql_query):
-    # Todo:
-    # - apply the sql query on the postgresSQL database and return the matches
-
-    pass
+    user_db.execute(sql_query)
+    matches = user_db.fetchall()
+    return matches
 
 def process_rule_matches_by_row(mongo_db, rows, rule): # version of proces_rule_matches
     # Todo:
